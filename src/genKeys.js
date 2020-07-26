d9990fc969d0f0a1f38694861b10fadf3739ddd1
Andrew <you@example.com>
2020-07-26 18:53:21 -0500

    removed console logs and edited readme

--- src/genKeys.js
+++ src/genKeys.js
@@ -7,6 +7,7 @@ const bs58 = require("bs58");
 var ec = new EC("secp256k1");
 
 const genPrivateKey = () => {
+  //Key must be within the range 0 and p where p = 2^256 – 2^32 – 977 for secp256k1
   const max = Buffer.from(
     "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140",
     "hex"
@@ -19,7 +20,6 @@ const genPrivateKey = () => {
       isInvalid = false;
     }
   }
-  console.log("> Private key created: ", privateKey.toString("hex"));
   return privateKey;
 };
 
@@ -35,15 +35,11 @@ const createPublicAddress = (publicKey) => {
   let hash = sha256(Buffer.from(publicKey, "hex"));
   let publicKeyHash = new ripemd160().update(Buffer.from(hash, "hex")).digest();
 
-  console.log("publicKeyHash: ", publicKeyHash);
-
   const step1 = Buffer.from("00" + publicKeyHash.toString("hex"), "hex");
-  console.log("step 1", step1);
   const step2 = sha256(step1);
   const step3 = sha256(Buffer.from(step2, "hex"));
   const checksum = step3.substring(0, 8);
   const step4 = step1.toString("hex") + checksum;
-  console.log("step 4", step4);
   const address = bs58.encode(Buffer.from(step4, "hex"));
   return address;
 };
